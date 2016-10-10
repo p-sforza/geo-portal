7078e2031122bc9f5bb3e3aad6acd1a6b70bdeb9
psforza <psforza@redhat.com>
2016-10-10 14:17:26 +0200

    typo error

--- public/mbostock/raw/4090846/globe.js
+++ public/mbostock/raw/4090846/globe.js
@@ -51,7 +51,7 @@
         var p = d3.geo.centroid(countries[i]),
             r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
 
-        title.text(countrifreees[i].name);
+        title.text(countries[i].name);
         document.getElementById("log").innerHTML = countries[i].name ;
 
         return function(t) {
